f4ae6c196b861f57c3e714c1fb25497021554a4b
Илья <63155683+ilya-uzun@users.noreply.github.com>
2022-03-14 16:30:00 +0500

    Update NomCommand.java

--- src/main/java/telegram/nonCommand/NomCommand.java
+++ src/main/java/telegram/nonCommand/NomCommand.java
@@ -3,6 +3,51 @@ package telegram.nonCommand;
 public class NomCommand {
 
     public String nonCommandExecute(Long chatId, String userName, String text){
-
+        Sttings settings;
+        String anwer;
+        try{
+            //создаём настройки из сообщения пользователя
+            settings = createSettings(text);
+            //добавляем настройки в мапу, чтобы потом их использовать для этого пользователя при генерации файла
+            saveUserSettings(chatId, settings);
+            answer = "Настройки обновлены. Вы всегда можете их посмотреть с помощью /settings";
+            //логируем событие, используя userName
+        } catch (IllegalSettingsException e) {
+            answer = e.getMessage() + 
+                      "\n\n Настройки не были изменены. Вы всегда можете их посмотреть с помощью /settings";
+            //логируем событие, используя userName
+        } catch (Exception e){
+            answer = "Простите, я не понимаю Вас. Возможно, Вам поможет /help";
+            //логируем событие, используя userName
+        } 
+        return answer;
     }
-}
+    
+   /**
+     * Создание настроек из полученного пользователем сообщения
+     * @param text текст сообщения
+     * @throws IllegalArgumentException пробрасывается, если сообщение пользователя не соответствует формату
+     */
+    private Settings createSettings(String text) throws IllegalArgumentException {
+        //отсекаем файлы, стикеры, гифки и прочий мусор
+        if (text == null) {
+            throw new IllegalArgumentException("Сообщение не является текстом");
+        }
+        //создаём из сообщения пользователя 3 числа-настройки (min, max, listCount) либо пробрасываем исключение о несоответствии сообщения требуемому формату
+        return new Settings(min, max, listCount);
+    }
+    
+   /**
+     * Добавление настроек пользователя в мапу, чтобы потом их использовать для этого пользователя при генерации файла
+     * Если настройки совпадают с дефолтными, они не сохраняются, чтобы впустую не раздувать мапу
+     * @param chatId id чата
+     * @param settings настройки
+     */
+    private void sevaUserSttings(Long chatId, Settings settings){
+        if (!settings.equals(Settings.getDefauldSettings())){
+            Bot.getUserSettings().put(chatId. settings);
+        } else {
+            Bot.getUserSettings().remove(chatId);
+        }
+    }
+}//NomCommand
